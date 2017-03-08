9a9dee53ceb9de11420b85f82963eaf7ea5083da
victoriadm13 <victoria.dotta@gmail.com>
2017-03-08 16:59:24 -0300

    Add files via upload

---
+++ server.R
@@ -0,0 +1,24 @@
+
+library(shiny)
+library(e1071)
+
+data <- read.csv("Diabetes.csv")
+data$Outcome = as.factor(data$Outcome)
+fit <- naiveBayes(Outcome ~ ., data = data)
+
+shinyServer(function(input, output) {
+   
+  output$text <- renderText({
+    inp <- data.frame(data)[NULL,1:8]
+    inp[1,] <- rbind(c(input$preg, input$gluc, input$bpres, input$sthick, input$insulin,
+    input$bmi, input$dpf, input$age))
+    prediction <- predict(fit, inp[,])
+    if (prediction == 1) {result <- "Yes."
+    } else {
+      result <- "No."
+    }
+    result
+  
+  })
+  
+})
